22e9cfd8127bd91ffd7961d368e4e45ad55a61a6
Electro255 <73359887+Electro255@users.noreply.github.com>
2021-05-22 22:44:01 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {tabNavigator} from 'react-navigation';
+import ReadStoryScreen from '../screens/ReadStoryScreen';
+import WriteStoryScreen from '../screens/WriteStoryScreen';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
